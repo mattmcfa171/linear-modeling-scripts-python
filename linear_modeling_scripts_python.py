9a6dffd4eaa80255f4182553e7685715e9bf6395
mattmcfa171 <107944172+mattmcfa171@users.noreply.github.com>
2022-06-30 12:06:06 -0400

    Add files via upload

---
+++ linear_modeling_scripts_python.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Homework Assignment: Linear Modeling Script in Python
+
+# ## Load Dependencies
+
+# In[1]:
+
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import sys
+
+
+# ## Load Data
+
+# In[2]:
+
+
+dataset = pd.read_csv(sys.argv[1])
+print(dataset)
+
+
+# ## Plot Data
+
+# In[3]:
+
+
+plt.scatter(dataset[['x']], dataset[['y']], color = 'red')
+plt.title('Y vs X')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.savefig('dataset.png')
+plt.clf()
+
+
+# ## Plot Linear Regression
+
+# In[4]:
+
+
+model = LinearRegression()
+model.fit(dataset[['x']], dataset[['y']])
+
+
+# In[5]:
+
+
+plt.scatter(dataset[['x']], dataset[['y']], color = 'red')
+plt.plot(dataset[['x']], model.predict(dataset[['x']]), color = 'blue')
+plt.title('Y vs X')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.savefig('linearmodel.png')
+plt.clf()
